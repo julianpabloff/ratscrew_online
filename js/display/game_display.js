d138f254a56461247e7354b88b0374801832217a
Julian Pabloff <julianpabloff@gmail.com>
2021-11-16 23:52:35 -0800

    Built drawCard function

--- js/display/game_display.js
+++ js/display/game_display.js
@@ -1,18 +1,4 @@
 const GameDisplay = function(d) {
-	/*
-
-┌───────────┐
-│ ♥         │
-│           │
-│           │
-│           │
-│           │
-│           │
-│           │
-└───────────┘
-
-
-	*/
 	this.setSize = function() {
 
 	}
@@ -20,9 +6,31 @@ const GameDisplay = function(d) {
 	game.enablePixels();
 	const designs = require('./cardDesigns.json');
 
+	const card = {value: 6, suit: 's'};
+	const suits = {h: '♥', c: '♣', d: '♦', s: '♠'};
+	const suitColors = {h: 'red', c: 'magenta', d: 'red', s: 'magenta'};
 	this.drawCard = function(x, y) {
 		game.outline('yellow');
-		game.pixel.drawGrid(designs[2], x, y);
+		d.buffer.setColor('cyan', 'reset');
+		game.draw('┌' + '─'.repeat(17) + '┐', x, y);
+		for (let i = 1; i < 11; i++) {
+			game.draw('│', x, y + i);
+			game.draw('│', x + 18, y + i);
+		}
+		game.draw('└' + '─'.repeat(17) + '┘', x, y + 11);
+		const suitChar = suits[card.suit];
+		if (suitChar) {
+			d.buffer.setFg(suitColors[card.suit]);
+			for (let i = 1; i < 9; i++) {
+				game.draw(suitChar, x + 2 * i, y + 1);
+				game.draw(suitChar, x + 2 * i, y + 10);
+			}
+		} else {
+			d.buffer.setFg('red');
+			game.draw('J O K E R', x + 5, y + 1);
+			game.draw('J O K E R', x + 5, y + 10);
+		}
+		game.pixel.drawGrid(designs[card.value - 1], x + 2, y + 2);
 		game.render();
 	}
 }
